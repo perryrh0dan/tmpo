762a95cc3f4ca2ccfba3734f4fcee1945a46d774
perryrh0dan <thomaspoehlmann96@googlemail.com>
2020-07-08 08:24:07 +0200

    feat(scripts): implement before_install script

--- src/repository/template/meta.rs
+++ src/repository/template/meta.rs
@@ -5,12 +5,18 @@ use std::path::Path;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Meta  {
+pub struct Meta {
   pub kind: Option<String>,
   pub name: Option<String>,
   pub description: Option<String>,
+  pub scripts: Option<Scripts>,
   pub extend: Option<Vec<String>>,
   pub exclude: Option<Vec<String>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Scripts {
+  pub before_install: Option<String>,
 }
 
 pub fn load_meta(dir: &str) -> Result<Meta, Error> {
@@ -23,6 +29,7 @@ pub fn load_meta(dir: &str) -> Result<Meta, Error> {
       description: None,
       extend: None,
       exclude: None,
+      scripts: None,
     };
     return Ok(meta);
   }
