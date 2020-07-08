e70a75ebf980cfc3fc76c54db79534cdaba5a015
perryrh0dan <thomaspoehlmann96@googlemail.com>
2020-07-08 09:29:58 +0200

    test(travis): sett cross version manual

--- scripts/install.sh
+++ scripts/install.sh
@@ -31,11 +31,15 @@ main() {
     esac
 
     # This fetches latest stable release
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
-                       | cut -d/ -f3 \
-                       | grep -E '^v[0.1.0-9.]+$' \
-                       | $sort --version-sort \
-                       | tail -n1)
+    # local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+    #                    | cut -d/ -f3 \
+    #                    | grep -E '^v[0.1.0-9.]+$' \
+    #                    | $sort --version-sort \
+    #                    | tail -n1)
+
+
+    local tag='v0.2.1'
+
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
