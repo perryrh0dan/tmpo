1190644b9225431fbfe1f8781a2c12a53617f7b2
perryrh0dan <thomaspoehlmann96@googlemail.com>
2020-08-17 21:18:27 +0200

    fix(travis): windows build

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -18,10 +18,10 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin tmpo --target $TARGET --release
+    cross rustc --bin $CRATE_NAME --target $TARGET --release
 
     # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/tmpo $stage/
+    cp target/$TARGET/release/$CRATE_NAME $stage/ || cp target/$TARGET/release/$CRATE_NAME.exe $stage/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
