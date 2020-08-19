6c564efda27566b1a1e629313006a3f94ec5dcd5
perryrh0dan <thomaspoehlmann96@googlemail.com>
2020-08-19 14:32:08 +0200

    feat(update): better auto update logic

--- src/action/default/update.rs
+++ src/action/default/update.rs
@@ -1,24 +1,65 @@
-extern crate self_update;
-use self_update::cargo_crate_version;
+use std::fs::File;
 
-pub fn update() {
-  let update = self_update::backends::github::Update::configure()
+extern crate tar;
+extern crate self_update;
+extern crate flate2;
+use clap::{crate_version};
+use tar::Archive;
+use flate2::read::GzDecoder;
+
+
+pub fn update() -> Result<(), Box<::std::error::Error>> {
+  let releases = self_update::backends::github::ReleaseList::configure()
     .repo_owner("perryrh0dan")
     .repo_name("tmpo")
-    .bin_name("tmpo")
-    .show_download_progress(true)
-    .current_version(cargo_crate_version!())
-    .build();
+    .build()?
+    .fetch()?;
 
-  if !update.is_ok() {
-    return;
+  // check version 
+  let version = crate_version!();
+  if releases[0].version == version {
+    println!("Tmpo is already up to date");
+    return Ok(());
+  }
+
+  let asset = releases[0].asset_for(&self_update::get_target()).unwrap();
+
+  let tmp_dir = tempfile::Builder::new().tempdir_in(::std::env::current_dir()?)?;
+  let tmp_tarball_path = tmp_dir.path().join(&asset.name);
+  std::fs::File::create(&tmp_tarball_path)?;
+  let tmp_tarball = std::fs::OpenOptions::new().create(true).append(true).open(&tmp_tarball_path).unwrap();
+
+  // download asset
+  let mut headers = reqwest::header::HeaderMap::new();
+  headers.insert(reqwest::header::ACCEPT, "application/octet-stream".parse().unwrap());
+  match self_update::Download::from_url(&asset.download_url).show_progress(true).set_headers(headers).download_to(&tmp_tarball) {
+    Ok(_) => (),
+    Err(error) => println!("{}", error)
   };
 
-  let status = update.unwrap().update();
-
-  if !status.is_ok() {
-    return;
+  // extract tar.gz archive
+  let tar_gz = File::open(tmp_tarball_path).unwrap();
+  let tar = GzDecoder::new(tar_gz);
+  let mut archive = Archive::new(tar);
+  match archive.unpack(&tmp_dir) {
+    Ok(_) => (),
+    Err(error) => println!("{}", error)
   };
 
-  println!("Update status: `{}`!", status.unwrap().version());
+  // move file to current executable
+  let bin_name = "tmpo";
+  let tmp_file = tmp_dir.path().join("replacement_tmp");
+  let bin_path = tmp_dir.path().join(bin_name);
+  self_update::Move::from_source(&bin_path)
+    .replace_using_temp(&tmp_file)
+    .to_dest(&::std::env::current_exe()?)?;
+
+  // remove tmp folder
+  match std::fs::remove_dir_all(tmp_dir) {
+    Ok(_) => (),
+    Err(error) => println!("{}", error),
+  };
+
+  println!("Successfully updated tmpo");
+  Ok(())
 }
