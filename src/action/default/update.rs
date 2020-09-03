42fc116a69cdfd8ac2d0a4d09cc6a284072c27d8
Thomas Pöhlmann <thomaspoehlmann96@googlemail.com>
2020-09-03 16:24:50 +0200

    fix(update): use windows_gnu binary for windows_msvc

--- src/action/default/update.rs
+++ src/action/default/update.rs
@@ -1,14 +1,14 @@
 use crate::cli::input::confirm;
+use crate::out;
 use crate::update;
 
 pub fn update() {
-  let asset = match update::check_version(true) {
+  let asset = match update::check_version(false) {
     Some(asset) => asset,
-    None => return,
+    None => return out::info::no_app_update(),
   };
 
   // user input
-
   let update = confirm("The new release will be downloaded/extraced and the existing binary will be replaced.\nDo you want to continue?");
   if !update {
       return;
