f319ee729f7b1cbc721e3795859a0f74e67783bb
perryrh0dan <thomaspoehlmann96@googlemail.com>
2020-12-08 19:01:21 +0100

    fix(migrate): add new line in config

--- src/migration/two.rs
+++ src/migration/two.rs
@@ -15,6 +15,10 @@ fn configuration() {
 
   let config = config::get_default_config();
 
+  if let Err(e) = writeln!(file, "") {
+    eprintln!("Couldn't write to file: {}", e);
+  }
+
   if let Err(e) = writeln!(file, "version: 2.0.0") {
     eprintln!("Couldn't write to file: {}", e);
   }
