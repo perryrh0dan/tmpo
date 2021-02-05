9aeddf14d18ee7e0f31e405109f1781689e85a04
perryrh0dan <thomaspoehlmann96@googlemail.com>
2021-02-05 23:47:03 +0100

    fix(gitignore): add src/out again

---
+++ src/out/error.rs
@@ -0,0 +1,30 @@
+use colored::Colorize;
+
+pub fn no_repositories() {
+  let text = format!(
+    "{}\nUse {} to add an repository.",
+    "No repositories are maintained!".red(),
+    "tmpo repository add".green()
+  );
+  eprintln!("{}", text);
+}
+
+pub fn no_templates(repository_name: &str) {
+  let text = format!("No templates found in repository: {}", repository_name);
+  eprintln!("{}", text);
+}
+
+pub fn template_not_found() {
+  let text = format!("Unable to load template! Error: Not found");
+  eprintln!("{}", text);
+}
+
+pub fn repository_exists(repository: &str) {
+  let text = format!("Repository: {} already exists", repository);
+  eprintln!("{}", text);
+}
+
+pub fn template_exists(template: &str) {
+  let text = format!("Template: {} already exists", template);
+  eprintln!("{}", text);
+}
---
+++ src/out/success.rs
@@ -0,0 +1,38 @@
+use colored::Colorize;
+
+pub fn app_updated() {
+  let text = format!("Successful updated app").green();
+  println!("{}", text);
+}
+
+pub fn workspace_created(name: &str) {
+  let text = format!("Created workspace: {}", name).green();
+  println!("{}", text);
+}
+
+pub fn template_created(path: &str) {
+  let text = format!("Created template: {}", path).green();
+  println!("{}", text);
+}
+
+pub fn repository_added(name: &str) {
+  let text = format!("Added repository: {}", name).green();
+  println!("{}", text);
+}
+
+pub fn repository_removed(name: &str) {
+  let text = format!("Removed repository: {}", name).green();
+  println!("{}", text);
+}
+
+pub fn remote_repository_created(name: &str, target: &str) {
+  let title = format!("Created remote repository: {} under: {}", name, target).green();
+  let text = "Navigate into the repository and start adding new templates.\nTo use the repository push it to gitlab or github and add it via tmpo repository add.";
+  println!("{}", title);
+  println!("{}", text);
+}
+
+pub fn template_tested() {
+  let text = format!("Test successful").green();
+  println!("{}", text);
+}
