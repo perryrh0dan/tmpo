e264231bff0250eca010427d83819782d309c961
perryrh0dan <thomaspoehlmann96@googlemail.com>
2021-05-01 11:46:15 +0200

    fix(cargo): downgrad libgit2-sys

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,15 +13,18 @@ build() {
 
   if [ $target = "x86_64-pc-windows-gnu" ]
   then
-    cp target/$target/release/$CRATE_NAME.exe stage/
+    cp target/$target/release/$CRATE_NAME.exe staging/
   else
-    cp target/$target/release/$CRATE_NAME stage/
+    cp target/$target/release/$CRATE_NAME staging/
   fi
-  cd stage
-  tar czf $CRATE_NAME-$TAG-$target.tar.gz *
 
-  cp $CRATE_NAME-$TAG-$target.tar.gz ../out
-  cd ..
+  echo "Create gziped tarball"
+  tar czf $CRATE_NAME-$TAG-$target.tar.gz staging/*
+
+  echo "Copy tarball to out"
+  cp $CRATE_NAME-$TAG-$target.tar.gz out
+
+  echo "Remove staging directory"
   rm -r staging
 }
 
